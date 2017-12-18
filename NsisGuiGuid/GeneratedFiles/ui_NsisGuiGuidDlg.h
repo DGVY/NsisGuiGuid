f5625f878ead23ca19faa6b8b06c1ce833da8127
李珂 <DGVY@outlook.com>
2017-12-18 15:14:58 +0800

    更改项目结构

--- NsisGuiGuid/GeneratedFiles/ui_NsisGuiGuidDlg.h
+++ NsisGuiGuid/GeneratedFiles/ui_NsisGuiGuidDlg.h
@@ -14,19 +14,50 @@
 #include <QtWidgets/QApplication>
 #include <QtWidgets/QButtonGroup>
 #include <QtWidgets/QDialog>
+#include <QtWidgets/QGroupBox>
 #include <QtWidgets/QHeaderView>
+#include <QtWidgets/QVBoxLayout>
 
 QT_BEGIN_NAMESPACE
 
 class Ui_NsisGuiGuidDlgClass
 {
 public:
+    QVBoxLayout *verticalLayout;
+    QGroupBox *GBTypeSelect;
+    QGroupBox *GBBaseCfg;
+    QGroupBox *GBAdvancedCfg;
+    QGroupBox *GBCompile;
 
     void setupUi(QDialog *NsisGuiGuidDlgClass)
     {
         if (NsisGuiGuidDlgClass->objectName().isEmpty())
             NsisGuiGuidDlgClass->setObjectName(QStringLiteral("NsisGuiGuidDlgClass"));
         NsisGuiGuidDlgClass->resize(600, 400);
+        verticalLayout = new QVBoxLayout(NsisGuiGuidDlgClass);
+        verticalLayout->setSpacing(6);
+        verticalLayout->setContentsMargins(11, 11, 11, 11);
+        verticalLayout->setObjectName(QStringLiteral("verticalLayout"));
+        GBTypeSelect = new QGroupBox(NsisGuiGuidDlgClass);
+        GBTypeSelect->setObjectName(QStringLiteral("GBTypeSelect"));
+
+        verticalLayout->addWidget(GBTypeSelect);
+
+        GBBaseCfg = new QGroupBox(NsisGuiGuidDlgClass);
+        GBBaseCfg->setObjectName(QStringLiteral("GBBaseCfg"));
+
+        verticalLayout->addWidget(GBBaseCfg);
+
+        GBAdvancedCfg = new QGroupBox(NsisGuiGuidDlgClass);
+        GBAdvancedCfg->setObjectName(QStringLiteral("GBAdvancedCfg"));
+
+        verticalLayout->addWidget(GBAdvancedCfg);
+
+        GBCompile = new QGroupBox(NsisGuiGuidDlgClass);
+        GBCompile->setObjectName(QStringLiteral("GBCompile"));
+
+        verticalLayout->addWidget(GBCompile);
+
 
         retranslateUi(NsisGuiGuidDlgClass);
 
@@ -36,6 +67,10 @@ public:
     void retranslateUi(QDialog *NsisGuiGuidDlgClass)
     {
         NsisGuiGuidDlgClass->setWindowTitle(QApplication::translate("NsisGuiGuidDlgClass", "NsisGuiGuidDlg", Q_NULLPTR));
+        GBTypeSelect->setTitle(QApplication::translate("NsisGuiGuidDlgClass", "\347\261\273\345\236\213\351\200\211\346\213\251", Q_NULLPTR));
+        GBBaseCfg->setTitle(QApplication::translate("NsisGuiGuidDlgClass", "\345\237\272\347\241\200\351\205\215\347\275\256", Q_NULLPTR));
+        GBAdvancedCfg->setTitle(QApplication::translate("NsisGuiGuidDlgClass", "\351\253\230\347\272\247\351\205\215\347\275\256", Q_NULLPTR));
+        GBCompile->setTitle(QApplication::translate("NsisGuiGuidDlgClass", "\345\216\213\347\274\251\347\274\226\350\257\221", Q_NULLPTR));
     } // retranslateUi
 
 };
